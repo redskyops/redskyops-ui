a9078482a51af6f8c428f7240b42122119185a89
Iyad Bitar <iyad.bitar@gmail.com>
2019-10-30 17:09:43 -0400

    adding dev proxy configuration

---
+++ src/setupProxy.js
@@ -0,0 +1,36 @@
+const proxy = require('http-proxy-middleware')
+const fetch = require('node-fetch')
+
+const REDSKY_ADDRESS = 'https://latest-master.carbonrelay.dev'
+const REDSKY_OAUTH2_CLIENT_ID = 'FhkZ45SY4ALycMGRdhfewWQKjYFsl70l'
+const REDSKY_OAUTH2_CLIENT_SECRET =
+  'QieY0Y1FV0JuugCwPKJ3hFe91fCPVCh3C7Dso3cQ8Lwgmzh3JJTsxtRA3V6C2vdN'
+
+let token
+let tokenType
+
+fetch(`${REDSKY_ADDRESS}/auth/token/`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: `client_id=${REDSKY_OAUTH2_CLIENT_ID}&client_secret=${REDSKY_OAUTH2_CLIENT_SECRET}`,
+})
+  .then(res => res.json())
+  .then(tokenRes => {
+    token = tokenRes.access_token
+    tokenType = tokenRes.token_type
+  })
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    proxy({
+      target: `${REDSKY_ADDRESS}/api`,
+      changeOrigin: true,
+      headers: {
+        Authorization: `${tokenType} ${token}`,
+      },
+    }),
+  )
+}
