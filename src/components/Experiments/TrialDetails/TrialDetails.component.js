e57ef737788fc5c6f8ac53abe853581a14353a36
Iyad Bitar <iyad.bitar@gmail.com>
2019-11-09 09:41:34 -0500

    adding unit test for TrialDetails components

--- src/components/Experiments/TrialDetails/TrialDetails.component.js
+++ src/components/Experiments/TrialDetails/TrialDetails.component.js
@@ -25,7 +25,11 @@ export const TrialDetails = (props: Props) => {
   const renderParameters = () => {
     return trial.assignments.map(para => {
       return (
-        <div className={style.parameter} key={para.parameterName}>
+        <div
+          className={style.parameter}
+          key={para.parameterName}
+          data-dom-id={`trial-para-${para.parameterName}`}
+        >
           <span className={style.capitalize}>
             <strong>{para.parameterName.replace(/_/g, ' ')}: </strong>
             {para.value}
@@ -44,7 +48,11 @@ export const TrialDetails = (props: Props) => {
 
   return (
     <div className={style.trial}>
-      <button className={style.close} onClick={closeHandler}>
+      <button
+        className={style.close}
+        onClick={closeHandler}
+        data-dom-id="trial-close"
+      >
         <span className="material-icons">close</span>
       </button>
       <h3 className={style.h3}>
@@ -53,18 +61,19 @@ export const TrialDetails = (props: Props) => {
           <span className={`material-icons ${style.icon}`}>check_circle</span>
         )}
       </h3>
-      <div className={style.parameter}>
+      <div className={style.parameter} data-dom-id="trial-value-1">
         <strong className={style.capitalize}>
           {trial.values[0].metricName}:
         </strong>{' '}
         {trial.values[0].value}
       </div>
-      <div className={style.parameter}>
+      <div className={style.parameter} data-dom-id="trial-value-2">
         <strong className={style.capitalize}>
           {trial.values[1].metricName}:
         </strong>{' '}
         {trial.values[1].value}
       </div>
+
       <h3 className={style.h3}>Parameters</h3>
       {renderParameters()}
     </div>
---
+++ src/components/Experiments/TrialDetails/TrialDetails.test.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import {TrialDetails} from './TrialDetails.component'
+import expStub from '../../../services/_stubs/exp-data'
+import trialsStub from '../../../services/_stubs/trials-data'
+
+describe('Component: TrialDetails', () => {
+  let wrapper
+  const props = {
+    parameters: expStub.experiments[0].parameters,
+    trial: trialsStub.trials[2],
+    closeHandler: jest.fn(),
+  }
+
+  beforeEach(() => {
+    props.closeHandler.mockClear()
+  })
+
+  it('should render TrialDetails component', () => {
+    wrapper = shallow(<TrialDetails {...props} />)
+    expect(wrapper).toHaveLength(1)
+    wrapper.unmount()
+  })
+
+  it('should render close button', () => {
+    wrapper = shallow(<TrialDetails {...props} />)
+    expect(wrapper.find('[data-dom-id="trial-close"]')).toHaveLength(1)
+    wrapper.unmount()
+  })
+
+  it('should call closeHandler on close click', () => {
+    wrapper = shallow(<TrialDetails {...props} />)
+    wrapper.find('[data-dom-id="trial-close"]').simulate('click')
+    expect(props.closeHandler).toHaveBeenCalledTimes(1)
+    wrapper.unmount()
+  })
+
+  it('should render values', () => {
+    wrapper = shallow(<TrialDetails {...props} />)
+
+    expect(wrapper.find('[data-dom-id="trial-value-1"]')).toHaveLength(1)
+    expect(wrapper.find('[data-dom-id="trial-value-1"]').text()).toBe(
+      `${props.trial.values[0].metricName}: ${props.trial.values[0].value}`,
+    )
+    expect(wrapper.find('[data-dom-id="trial-value-2"]')).toHaveLength(1)
+    expect(wrapper.find('[data-dom-id="trial-value-2"]').text()).toBe(
+      `${props.trial.values[1].metricName}: ${props.trial.values[1].value}`,
+    )
+    wrapper.unmount()
+  })
+
+  it('should render parameters with their names and values', () => {
+    wrapper = shallow(<TrialDetails {...props} />)
+    props.trial.assignments.forEach(para => {
+      const paraDom = wrapper.find(
+        `[data-dom-id="trial-para-${para.parameterName}"]`,
+      )
+      expect(paraDom).toHaveLength(1)
+      expect(paraDom.text()).toContain(para.parameterName)
+      expect(paraDom.text()).toContain(para.value)
+    })
+    wrapper.unmount()
+  })
+
+  it('should render parameters with RangeIndicator', () => {
+    wrapper = shallow(<TrialDetails {...props} />)
+    props.trial.assignments.forEach(para => {
+      const paraDom = wrapper.find(
+        `[data-dom-id="trial-para-${para.parameterName}"]`,
+      )
+      expect(paraDom).toHaveLength(1)
+      expect(paraDom.find('RangeIndicator')).toHaveLength(1)
+      const indicatorProps = paraDom.find('RangeIndicator').props()
+      const {bounds} = props.parameters.find(p => p.name === para.parameterName)
+      expect(indicatorProps).toHaveProperty('value', para.value)
+      expect(indicatorProps).toHaveProperty('min', bounds.min)
+    })
+    wrapper.unmount()
+  })
+})
