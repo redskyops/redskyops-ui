7c1f5fd67b0c5575df89bf80a5d3f6d2ebb8d563
Iyad Bitar <iyad.bitar@gmail.com>
2019-12-17 18:31:13 -0500

    fixing limited experiments response

--- src/components/Charts/ChartProps.type.js
+++ src/components/Charts/ChartProps.type.js
@@ -1,11 +1,9 @@
 import {TypeTrials, TypeActiveTrial} from '../../context/DefaultState'
 
-type ChartPropsType = {
+export type ChartPropsType = {
   trials: TypeTrials,
   activeTrial?: TypeActiveTrial,
   xAxisMetricName: string,
   yAxisMetricName: string,
   selectTrialHandler: Function,
 }
-
-export default ChartPropsType
