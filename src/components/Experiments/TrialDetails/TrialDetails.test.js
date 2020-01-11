5a88d1e412b681d35f79dba773e46b6ee7394fa6
Iyad Bitar <iyad.bitar@gmail.com>
2020-01-11 07:58:16 -0500

    more unit test for TrialDetails

--- src/components/Experiments/TrialDetails/TrialDetails.test.js
+++ src/components/Experiments/TrialDetails/TrialDetails.test.js
@@ -78,4 +78,43 @@ describe('Component: TrialDetails', () => {
     })
     wrapper.unmount()
   })
+
+  it('should render withouth RangeIndicator if parameter is missing from props', () => {
+    const localProps = {...props}
+    delete localProps.parameters
+    wrapper = shallow(<TrialDetails {...localProps} />)
+    expect(wrapper.find('.parameter')).toHaveLength(4)
+    expect(wrapper.find('RangeIndicator')).toHaveLength(0)
+    wrapper.unmount()
+  })
+
+  it('should set right CSS class if trial is optimal', () => {
+    const localProps = {
+      ...props,
+      trial: {
+        ...props.trial,
+        labels: {best: 'true'},
+      },
+    }
+    wrapper = shallow(<TrialDetails {...localProps} />)
+    wrapper.find('RangeIndicator').forEach(indicator => {
+      expect(indicator.props()).toHaveProperty('indecatorClass')
+    })
+    wrapper.unmount()
+  })
+
+  it('should render best icon if trial is optimal', () => {
+    const localProps = {
+      ...props,
+      trial: {
+        ...props.trial,
+        labels: {best: 'true'},
+      },
+    }
+    wrapper = shallow(<TrialDetails {...localProps} />)
+    expect(wrapper.find('.material-icons')).toHaveLength(2)
+    wrapper.setProps(props)
+    expect(wrapper.find('.material-icons')).toHaveLength(1)
+    wrapper.unmount()
+  })
 })
