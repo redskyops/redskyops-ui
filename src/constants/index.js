c7096143bbd96570772bab10e9fbb0b587b60ea5
Iyad Bitar <iyad.bitar@gmail.com>
2020-02-12 17:06:04 -0500

    fixing lint error

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
-export type TypeAxisType = AXIS_TYPE.METIC | AXIS_TYPE.PARAMETE
+export type TypeAxisType = 'parameter' | 'metric'
+
 export const AXIS_TYPE: TypeAxisType = {
   PARAMETER: 'parameter',
   METRIC: 'metric',
