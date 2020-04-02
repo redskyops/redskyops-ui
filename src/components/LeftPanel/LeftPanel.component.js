acf30ce706d12f3d1f87ea4c9a8953679ab6348e
Iyad Bitar <iyad.bitar@gmail.com>
2020-04-02 17:56:18 -0400

    collapsable left panel

--- src/components/LeftPanel/LeftPanel.component.js
+++ src/components/LeftPanel/LeftPanel.component.js
@@ -2,64 +2,88 @@ import React from 'react'
 
 import ExperimentsList from '../Experiments/ExperimentsList/ExperimentsList.component'
 import {connectWithState} from '../../context/StateContext'
-import {TypeExperiments} from '../../context/DefaultState'
+import {TypeExperiments, TypeLeftPanel} from '../../context/DefaultState'
 
 import style from './LeftPanel.module.scss'
 import Icon from '../Icon/Icon.component'
 
 type TypeProps = {
   experiments: TypeExperiments,
+  leftPanel: TypeLeftPanel,
   updateState: () => {},
 }
-console.log('slsll')
+
 export const LeftPanel = (props: TypeProps) => {
-  const {experiments, updateState} = props
+  const {leftPanel, experiments, updateState} = props
+  const togglePanel = () => {
+    updateState({
+      leftPanel: {
+        ...leftPanel,
+        show: !leftPanel.show,
+      },
+    })
+  }
   return (
     <div className={style.leftPanel}>
-      <div className={style.search}>
-        <input
-          type="text"
-          className={style.searchInput}
-          placeholder="FILTER EXPERIMENTS"
-          value={experiments.filter.name}
-          onChange={e => {
-            updateState({
-              experiments: {
-                ...experiments,
-                filter: {
-                  ...experiments.filter,
-                  name: e.target.value,
-                },
-              },
-            })
-          }}
-        />
-        {!experiments.filter.name && (
-          <Icon icon="search" width={24} cssClass={style.icon} />
-        )}
-        {experiments.filter.name && (
-          <button
-            className={style.searchClearBtn}
-            data-dom-id="left-panel-clear"
-            onClick={() => {
-              updateState({
-                experiments: {
-                  ...experiments,
-                  filter: {
-                    ...experiments.filter,
-                    name: '',
+      <button
+        style={{fontSize: '24px', paddingLeft: '8px', marginTop: '15px'}}
+        className={`${style.button} fa fa-angle-double-${
+          leftPanel.show ? 'left' : 'right'
+        }`}
+        onClick={togglePanel}
+      />
+      <div
+        className={`${style.outter} ${
+          leftPanel.show ? style.expand : style.collapse
+        }`}
+      >
+        <div className={style.inner}>
+          <div className={style.search}>
+            <input
+              type="text"
+              className={style.searchInput}
+              placeholder="FILTER EXPERIMENTS"
+              value={experiments.filter.name}
+              onChange={e => {
+                updateState({
+                  experiments: {
+                    ...experiments,
+                    filter: {
+                      ...experiments.filter,
+                      name: e.target.value,
+                    },
                   },
-                },
-              })
-            }}
-          >
-            <Icon icon="circleX" width={24} cssClass={style.icon} />
-          </button>
-        )}
+                })
+              }}
+            />
+            {!experiments.filter.name && (
+              <Icon icon="search" width={24} cssClass={style.icon} />
+            )}
+            {experiments.filter.name && (
+              <button
+                className={style.searchClearBtn}
+                data-dom-id="left-panel-clear"
+                onClick={() => {
+                  updateState({
+                    experiments: {
+                      ...experiments,
+                      filter: {
+                        ...experiments.filter,
+                        name: '',
+                      },
+                    },
+                  })
+                }}
+              >
+                <Icon icon="circleX" width={24} cssClass={style.icon} />
+              </button>
+            )}
+          </div>
+          <ExperimentsList />
+        </div>
       </div>
-      <ExperimentsList />
     </div>
   )
 }
 
-export default connectWithState(LeftPanel, [])
+export default connectWithState(LeftPanel, ['experiments', 'leftPanel'])
