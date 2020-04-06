23b3f645d391dba74c07e20fb4c4a58f6b6b5c35
Ruba Hassan <rubahassan@Rubas-MacBook-Pro.local>
2020-04-06 15:27:18 -0700

    added the RED SKY OPS icon and changed the layout of the header

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -2,24 +2,28 @@ import React from 'react'
 import {withRouter} from 'react-router-dom'
 
 import style from './Header.module.scss'
+import logo from '../../assets/images/redskyop-clouds.png'
 
 export const Header = () => {
   return (
     <header className={style.header}>
-      <div className="col-lg-12">
-        <a style={{color: '#000000'}} href="/" className="button">
-          <h1 className={style.h1}>RED SKY OPS</h1>
-          <h4 className={style.h4}>VERSION 2.0</h4>
+      <div style={{paddingLeft: '20px', flex: '3'}}>
+        <a style={{color: '#f45266'}} href="/" className="button">
+          <h1 className={style.h1}>
+            <img className={style.logo} src={logo} alt="RED SKY OPS" />
+            RED SKY OPS
+          </h1>
         </a>
-        <div className={style.a}>
-          <a
-            style={{color: '#000000', fontWeight: 'bold'}}
-            href="/helpDocs"
-            className="button"
-          >
-            Help Center
-          </a>
-        </div>
+      </div>
+      <div stlye={{flex: '1'}}></div>
+      <div className={style.a} style={{flex: '2'}}>
+        <a
+          style={{color: '#000000', fontWeight: 'bold'}}
+          href="/helpDocs"
+          className="button"
+        >
+          Help Center
+        </a>
       </div>
     </header>
   )
