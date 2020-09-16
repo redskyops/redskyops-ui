3bbdc17fde1c39c7c8c5f795f9ba54bd3fff93a5
Jeremy Gustie <jeremy@carbonrelay.com>
2020-09-16 11:17:17 -0400

    Update Go test code
    Use this with `redskyctl results --address :8888 --idle-timeout 0` to test the generated UI

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	http.Handle("/ui/", http.StripPrefix("/ui/", http.FileServer(ui.Assets)))
-	http.Handle("/api/", &httputil.ReverseProxy{
+	http.Handle("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	http.Handle("/v1/", &httputil.ReverseProxy{
 		Director: func(req *http.Request) {
 			req.Header.Set("X-Forwarded-Proto", "http")
 			req.Header.Set("X-Forwarded-Host", req.Host)
